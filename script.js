a0a46e8a07873f8b87c0f2aea1e14adecd1f3184
TommyN987 <tomas.nagy.tn@gmail.com>
2022-04-12 14:24:12 +0200

    add search functionality

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-// ******************************************************* 
+// *******************************************************
 // GET DOM ELEMENTS
 // *******************************************************
 
 const time = document.getElementById('time');
 const date = document.getElementById('date');
+const timezone = document.getElementById('time-zone');
+const searchInput = document.getElementById('search-input');
+const btnSearch = document.getElementById('btn-search');
 const currentTemp = document.getElementById('current-temp');
 const sky = document.getElementById('sky');
 const windSpeed = document.getElementById('wind-speed');
@@ -11,9 +14,12 @@ const humidity = document.getElementById('humidity');
 const pressure = document.getElementById('pressure');
 const sunrise = document.getElementById('sunrise');
 const sunset = document.getElementById('sunset');
-const timezone = document.getElementById('time-zone');
 
-// ******************************************************* 
+btnSearch.addEventListener('click', () => {
+  getSpecificWeather(searchInput.value);
+});
+
+// *******************************************************
 // GLOBAL VARIABLES
 // *******************************************************
 
@@ -25,7 +31,7 @@ const days = [
   'Wednesday',
   'Thursday',
   'Friday',
-  'Saturday'
+  'Saturday',
 ];
 const months = [
   'Jan',
@@ -39,14 +45,14 @@ const months = [
   'Sep',
   'Oct',
   'Nov',
-  'Dec'
+  'Dec',
 ];
 
-// ******************************************************* 
+// *******************************************************
 // GET CURRENT TIME AND DATE
 // *******************************************************
 
-(function getTimeAndDate () {
+(function getTimeAndDate() {
   setInterval(() => {
     const currentTime = new Date();
     const month = currentTime.getMonth();
@@ -57,11 +63,19 @@ const months = [
     const hoursIn12HrFormat = hour >= 13 ? hour % 12 : hour;
     const minutes = currentTime.getMinutes();
 
-    time.innerHTML = `${hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat}:${(minutes < 10 ? '0' + minutes : minutes)} <span id="am-pm">${ampm}</span>`
+    time.innerHTML = `${
+      hoursIn12HrFormat < 10 ? '0' + hoursIn12HrFormat : hoursIn12HrFormat
+    }:${
+      minutes < 10 ? '0' + minutes : minutes
+    } <span id="am-pm">${ampm}</span>`;
 
     date.innerHTML = `${days[day]}, ${currentdate} ${months[month]}`;
-  }, 1000)
+  }, 1000);
 })();
+
+// *******************************************************
+// CURRENT WEATHER
+// *******************************************************
 
 const showCurrentWeatherData = (data) => {
   timezone.innerText = data.timezone;
@@ -70,21 +84,53 @@ const showCurrentWeatherData = (data) => {
   windSpeed.innerText = `${data.current.wind_speed} m/s`;
   humidity.innerText = `${data.current.humidity}%`;
   pressure.innerText = data.current.pressure;
-  sunrise.innerText = window.moment(data.current.sunrise * 1000).format('HH:mm a');
-  sunset.innerText = window.moment(data.current.sunset * 1000).format('HH:mm a');
-}
+  sunrise.innerText = window
+    .moment(data.current.sunrise * 1000)
+    .format('HH:mm a');
+  sunset.innerText = window
+    .moment(data.current.sunset * 1000)
+    .format('HH:mm a');
+};
 
 const getCurrentWeatherData = () => {
   navigator.geolocation.getCurrentPosition((success) => {
-    console.log(success);
-    
-    fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${success.coords.latitude}&lon=${success.coords.longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`)
+
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${success.coords.latitude}&lon=${success.coords.longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+    )
       .then((resolution) => resolution.json())
       .then((data) => {
         console.log(data);
         showCurrentWeatherData(data);
-      })
+      });
+  });
+};
+
+getCurrentWeatherData();
+
+function getSpecificWeather (city) {
+  fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`)
+  .then((resolution) => resolution.json())
+  .then((data) => {
+    console.log(data);
+    timezone.innerText = data.name + ', ' + data.sys.country;
+    currentTemp.innerText = `${data.main.temp} °C`;
+    sky.innerText = data.weather[0].main;
+    windSpeed.innerText = `${data.wind.speed} m/s`;
+    humidity.innerText = `${data.main.humidity}%`;
+    pressure.innerText = data.main.pressure;
+    sunrise.innerText = window
+      .moment(data.sys.sunrise * 1000)
+      .format('HH:mm a');
+    sunset.innerText = window
+      .moment(data.sys.sunset * 1000)
+      .format('HH:mm a');
+    searchInput.value = ''
   })
 }
 
-getCurrentWeatherData();
+// getSpecificWeather('munich');
+
+function getUserSpecifiedWeather() {
+  console.log('weather')
+}
