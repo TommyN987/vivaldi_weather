c964ed47cbb01d161975c63a23316c03c9e25630
TommyN987 <tomas.nagy.tn@gmail.com>
2022-04-14 01:00:26 +0200

    clean up

--- script.js
+++ script.js
@@ -73,11 +73,64 @@ const months = [
 })();
 
 // *******************************************************
-// CURRENT LOCAL WEATHER
+// GET LOCAL WEATHER DATA
 // *******************************************************
 
-const displayLocalWeather = (data) => {
-  timezone.innerText = data.timezone;
+function getLocalWeather () {
+  
+  // GET GEOLOCATION
+  navigator.geolocation.getCurrentPosition((success) => {
+
+    // GET API DATA BASE ON GEOLOCATION COORDINATES
+    fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${success.coords.latitude}&lon=${success.coords.longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
+    )
+      .then((resolution) => resolution.json())
+      .then((data) => {
+        setMood(data);
+        displayCurrentWeather(data);
+        displayWeatherForecast(data.daily);
+      })
+      .catch((error) => {
+        alert(`Couldn't retrieve data for your location:
+        ${error}`);  
+      });
+
+    fetch(
+      `http://api.openweathermap.org/geo/1.0/reverse?lat=${success.coords.latitude}&lon=${success.coords.longitude}&limit=5&appid=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((geo) => {
+        console.log(geo);
+        timezone.innerText = geo[0].name + ', ' + geo[0].country;
+      })
+  });
+};
+
+// *******************************************************
+// GET WEATHER DATA FOR USER-SPECIFIED CITY
+// *******************************************************
+
+// GET API DATA
+async function getUserSearchedWeather (city) {
+  
+  // GET COORDINATES
+  const res = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${API_KEY}`);
+  const geoData = await res.json();
+  console.log(geoData);
+  
+  // FORECAST DATA
+  const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${geoData[0].lat}&lon=${geoData[0].lon}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`);
+  const forecastData = await forecastRes.json();
+  console.log(forecastData);
+  return { geoData, forecastData };
+}
+
+// *******************************************************
+// DISPLAY API DATA
+// *******************************************************
+
+function displayCurrentWeather (data) {
   currentTemp.innerText = `${data.current.temp} °C`;
   sky.innerText = data.current.weather[0].main;
   windSpeed.innerText = `${data.current.wind_speed} m/s`;
@@ -91,7 +144,7 @@ const displayLocalWeather = (data) => {
     .format('HH:mm a');
 };
 
-const displayWeatherForecast = (data) => {
+function displayWeatherForecast (data) {
   for (let i = 0; i < (data.length - 1); i++) {
     document.getElementById(`day-name-${i}`).innerText = window.moment(data[i].dt*1000).format('ddd');
     document.getElementById(`day-temp-${i}`).innerText = data[i].temp.day;
@@ -100,55 +153,56 @@ const displayWeatherForecast = (data) => {
   }
 };
 
-const getLocalWeather = () => {
-  navigator.geolocation.getCurrentPosition((success) => {
-    console.log(success);
-    fetch(
-      `https://api.openweathermap.org/data/2.5/onecall?lat=${success.coords.latitude}&lon=${success.coords.longitude}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`
-    )
-      .then((resolution) => resolution.json())
-      .then((data) => {
-        console.log(data);
-        setMood(data);
-        displayLocalWeather(data);
-        displayWeatherForecast(data.daily);
-      })
-      .catch((error) => {
-        alert(`Couldn't retrieve data for ${searchInput.value}:
-        ${error}`);  
-      });
-  });
-};
+function setMood(data) {
+  // SET BACKGROUND IMAGE
+  if (data.current.weather[0].main === 'Snow' || data.current.temp < 0) {
+    body.style.backgroundImage = `url(../img/snowy.jpg)`
+  } else if (data.current.weather[0].main === 'Clouds') {
+    body.style.backgroundImage = `url(../img/cloudy.jpg)`
+  } else if (data.current.weather[0].main === 'Clear') {
+    body.style.backgroundImage = `url(../img/sunny.jpg)`
+  } else if (data.current.weather[0].main === 'Rain' || data.current.weather[0].main === 'Drizzle') {
+    body.style.backgroundImage = `url(../img/rainy.jpg)`
+  } else if (data.current.weather[0].main === 'Thunderstorm') {
+    body.style.backgroundImage = `url(../img/stormy.jpg)`
+  } else if (data.current.weather[0].main === 'Mist' || data.current.weather[0].main === 'Haze' || data.current.weather[0].main === 'Fog') {
+    body.style.backgroundImage = `url(../img/misty.jpg)`
+  } else {
+    body.style.backgroundImage = `url(../img/default.jpg)`
+  }
 
-// getLocalWeather();
-
-// *******************************************************
-// WEATHER FOR USER-SPECIFIED CITY
-// *******************************************************
-
-// RETRIEVE DATA FROM API
-async function getUserSearchedWeather (city) {
-  
-  // CURRENT WEATHER DATA
-  const resolution = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`);
-  const currentData = await resolution.json();
-  
-  // FORECAST DATA
-  const forecastRes = await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${currentData.coord.lat}&lon=${currentData.coord.lon}&exclude=hourly,minutely&units=metric&appid=${API_KEY}`);
-  const forecastData = await forecastRes.json();
-  return forecastData;
+  // SET VIVALDI
+  if (data.current.weather[0].main === 'Thunderstorm' || data.current.weather[0].main === 'Rain') {
+    audio.src = './audio/storm.mp3';
+  } else if (data.current.temp < 5) {
+    audio.src = './audio/winter.mp3';
+  } else if (data.current.temp < 15) {
+    audio.src = './audio/autumn.mp3';
+  } else if (data.current.temp < 25) {
+    audio.src = './audio/spring.mp3';
+  } else {
+    audio.src = './audio/summer.mp3';
+  }
+  audio.muted = false;
+  audio.play();
 }
 
+function renderAll(data) {
+  timezone.innerText = data.geoData[0].name + ', ' + data.geoData[0].country;
+  setMood(data.forecastData);
+  displayCurrentWeather(data.forecastData);
+  displayWeatherForecast(data.forecastData.daily);
+}
+
+// *******************************************************
 // EVENT LISTENERS
+// *******************************************************
+
 btnSearch.addEventListener('click', () => {
   getUserSearchedWeather(searchInput.value)
     .then((result) => {
-      console.log(result);
-      locality.innerText = searchInput.value.toUpperCase();
       searchInput.value = '';
-      setMood(result);
-      displayLocalWeather(result);
-      displayWeatherForecast(result.daily);
+      renderAll(result);
     })
     .catch((error) => {
       alert(`Couldn't retrieve data for ${searchInput.value}:
@@ -160,12 +214,8 @@ searchInput.addEventListener('keydown', (e) => {
   if (e.key === 'Enter') {
     getUserSearchedWeather(searchInput.value)
     .then((result) => {
-      console.log(result);
-      locality.innerText = searchInput.value.toUpperCase();
       searchInput.value = '';
-      setMood(result);
-      displayLocalWeather(result);
-      displayWeatherForecast(result.daily);
+      renderAll(result);
     })
     .catch((error) => {
       alert(`Couldn't retrieve data for ${searchInput.value}:
@@ -174,35 +224,4 @@ searchInput.addEventListener('keydown', (e) => {
   }
 });
 
-function setMood(data) {
-  if (data.current.weather[0].main === 'Snow' || data.current.temp < 0) {
-    body.style.backgroundImage = `url(../img/snowy.jpg)`
-  } else if (data.current.weather[0].main === 'Clouds') {
-    body.style.backgroundImage = `url(../img/cloudy.jpg)`
-  } else if (data.current.weather[0].main === 'Clear') {
-    body.style.backgroundImage = `url(../img/sunny.jpg)`
-  } else if (data.current.weather[0].main === 'Rain' || data.current.weather[0].main === 'Drizzle') {
-    body.style.backgroundImage = `url(../img/rainy.jpg)`
-  } else if (data.current.weather[0].main === 'Thunderstorm') {
-    body.style.backgroundImage = `url(../img/stormy.jpg)`
-  } else {
-    body.style.backgroundImage = `url(../img/default.jpg)`
-  }
-
-  if (data.current.weather[0].main === 'Thunderstorm' || data.current.weather[0].main === 'Rain') {
-    audio.src = './audio/storm.mp3'
-    audio.play();
-  } else if (data.current.temp < 5) {
-    audio.src = './audio/winter.mp3'
-    audio.play();
-  } else if (data.current.temp < 15) {
-    audio.src = './audio/autumn.mp3'
-    audio.play();
-  } else if (data.current.temp < 25) {
-    audio.src = './audio/spring.mp3'
-    audio.play();
-  } else {
-    audio.src = './audio/summer.mp3'
-    audio.play();
-  }
-}
+// getLocalWeather();
